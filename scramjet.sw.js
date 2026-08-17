1166ac4f45c038c9d2533c30a5e9d9bb101c2740
donutzwebdev <donutz.web.dev@gmail.com>
2026-08-17 17:05:38 -0600

    Update scramjet.sw.js

--- scramjet.sw.js
+++ scramjet.sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@2.0.67-alpha.1/dist/scramjet.sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@1.1.0/dist/scramjet.sw.js');
