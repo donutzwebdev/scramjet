cc765cf363c9480cc573d820a838f4b04aeaa3b8
donutzwebdev <donutz.web.dev@gmail.com>
2026-08-17 17:09:44 -0600

    Update scramjet.sw.js

--- scramjet.sw.js
+++ scramjet.sw.js
@@ -1,1 +1,11 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@1.1.0/dist/scramjet.sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@1.1.0/dist/scramjet.all.js');
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sw = new ScramjetServiceWorker();
+
+self.addEventListener('fetch', async (ev) => {
+  await sw.loadConfig();
+  if (sw.route(ev)) {
+    ev.respondWith(sw.fetch(ev));
+  }
+});
