e20287f86fa45105675d0585e88fc66fa8065695
donutzwebdev <donutz.web.dev@gmail.com>
2026-08-18 20:39:50 -0600

    Update scramjet.sw.js

--- scramjet.sw.js
+++ scramjet.sw.js
@@ -3,14 +3,12 @@ importScripts("https://donutzwebdev.github.io/scramjet/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-async function handleRequest(event) {
-  await scramjet.loadConfig();
-  if (scramjet.route(event)) {
-    return scramjet.fetch(event);
-  }
-  return fetch(event.request);
-}
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event));
+  event.respondWith((async () => {
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+      return scramjet.fetch(event);
+    }
+    return fetch(event.request);
+  })());
 });
